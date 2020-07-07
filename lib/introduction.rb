db8166a9a80aba77d0c5805fedb14abe2531dd9e
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-07 00:18:07 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,8 +3,8 @@ def introduction (name)
   learning to program in #{language}."
 end
 
-describe "#introduction" do 
-  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
+describe "#introduction" do
+  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
   end
 end
