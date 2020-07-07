977be96f2835fdc9b940f9fdc4cd7dc0095df65b
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-07 00:08:29 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,6 @@
-name = "Josh"
-def introduction (name)
-  puts "Hi, my name is #{name}"
+def introduction (name, language)
+  puts "Hi, my name is #{name} and I am
+  learning to program in #{language}."
 end
+
+introduction ("Ashley","Ruby")
